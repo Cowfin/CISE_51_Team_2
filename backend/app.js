4942153fb9943fe5216a6d0492260a57cef332a2
Kelvin Wong <kakoo40@gmail.com>
2021-09-24 14:44:43 +1200

    Build functionality to run production build

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config({ path: "./config.env" });
+const path = require('path');
 const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
@@ -16,10 +18,21 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello world!'));
+//app.get('/', (req, res) => res.send('Hello world!'));
 
 // use Routes
 app.use('/api/record', records);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "..", "frontend", "build", "index.html"));
+    });
+} else {
+    app.get("/", (req,res) =>{
+        res.send("Api running");
+    });
+}
 
 const port = process.env.PORT || 8082;
 
